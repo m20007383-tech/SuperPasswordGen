c28cb5d038266b5f6469dfc14284deb43bc0d856
Maximus <m20007383@gmail.com>
2026-02-03 13:52:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ try:
     length = int(input("Введите длину: "))
 except ValueError:
     print("Ошибка! Используем длину по умолчанию (8).")
-    length = 16
+    length = 8
 
 password = ''.join(secrets.choice(pool) for _ in range(length))
 print(f"Ваш пароль: {password}")
+
 
